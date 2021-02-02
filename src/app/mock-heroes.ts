2296e5624b8f955ea3b559f0d7982c8bcab53753
Anchal Jaiswal <anchaljaiswal0116@gmail.com>
2021-02-02 15:51:14 +0530

    tour-of-heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 2, name: 'Dr Nice' },
+  { id: 3, name: 'Narco' },
+  { id: 4, name: 'Bombasto' },
+  { id: 5, name: 'Celeritas' },
+  { id: 6, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
